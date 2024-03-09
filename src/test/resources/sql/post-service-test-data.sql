4195734c64e99ff5166579087b066a0feebd2ce7
javamogi <javamogi@gmail.com>
2024-03-09 12:45:33 +0900

    postservice 메소드에 테스트 코드 추가

---
+++ src/test/resources/sql/post-service-test-data.sql
@@ -0,0 +1,6 @@
+insert into users (email, nickname, address, certification_code, status, last_login_at)
+values('pakoh200@naver.com', 'javamogi', 'Incheon', 'aaaaaa-aaaa-aaaa-aaaa-aaaaaaaa', 'ACTIVE', 0);
+insert into users (email, nickname, address, certification_code, status, last_login_at)
+values('pakoh300@naver.com', 'javamogi2', 'Incheon', 'aaaaaa-aaaa-aaaa-aaaa-aaaaaaab', 'PENDING', 0);
+insert into posts (content, created_at, modified_at, user_id)
+values('helloworld', 1678530673958, 0, 1);
