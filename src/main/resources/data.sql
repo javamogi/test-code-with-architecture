f3cc0e1f8f22a27a7fb22852e1cc5be12104231b
javamogi <javamogi@gmail.com>
2024-03-09 13:17:53 +0900

    userController 테스트 코드 추가

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-values(1, 'pakoh200@naver.com', 'javamogi', 'Incheon', 'aaaaaa-aaaa-aaaa-aaaa-aaaaaaaa', 'ACTIVE', 0);
-insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-values(2, 'pakoh300@naver.com', 'javamogi2', 'Incheon', 'aaaaaa-aaaa-aaaa-aaaa-aaaaaaab', 'PANDING', 0);
--- src/test/resources/sql/user-repository-test-data.sql
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -1,2 +1,2 @@
-insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+insert into users (id, email, nickname, address, certification_code, status, last_login_at)
 values(1, 'pakoh200@naver.com', 'javamogi', 'Incheon', 'aaaaaa-aaaa-aaaa-aaaa-aaaaaaaa', 'ACTIVE', 0);
