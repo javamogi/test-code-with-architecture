7dbb20037a581de1740a6ff274e71176de4b1aa4
javamogi <javamogi@gmail.com>
2024-03-09 13:39:18 +0900

    postController 및 2개의 컨트롤러에 테스트 코드 추가

---
+++ src/test/resources/sql/post-controller-test-data.sql
@@ -0,0 +1,4 @@
+insert into users (email, nickname, address, certification_code, status, last_login_at)
+values('pakoh200@naver.com', 'javamogi', 'Incheon', 'aaaaaa-aaaa-aaaa-aaaa-aaaaaaaa', 'ACTIVE', 0);
+insert into posts (content, created_at, modified_at, user_id)
+values('helloworld', 1678530673958, 0, 1);
---
+++ src/test/resources/sql/post-create-controller-test-data.sql
@@ -0,0 +1,4 @@
+insert into users (email, nickname, address, certification_code, status, last_login_at)
+values('pakoh200@naver.com', 'javamogi', 'Incheon', 'aaaaaa-aaaa-aaaa-aaaa-aaaaaaaa', 'ACTIVE', 0);
+insert into users (email, nickname, address, certification_code, status, last_login_at)
+values('pakoh300@naver.com', 'javamogi2', 'Incheon', 'aaaaaa-aaaa-aaaa-aaaa-aaaaaaab', 'PENDING', 0);
