70e27270a0fc81373f6c47e0fbfcda7f969521fb
javamogi <javamogi@gmail.com>
2024-03-10 17:32:52 +0900

    진짜 완

--- src/test/java/com/example/demo/post/controller/PostControllerTest.java
+++ src/test/java/com/example/demo/post/controller/PostControllerTest.java
@@ -5,27 +5,16 @@ import com.example.demo.mock.TestContainer;
 import com.example.demo.post.controller.response.PostResponse;
 import com.example.demo.post.domain.Post;
 import com.example.demo.post.domain.PostUpdate;
-import com.example.demo.user.controller.response.UserResponse;
 import com.example.demo.user.domain.User;
 import com.example.demo.user.domain.UserStatus;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlGroup;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class PostControllerTest {
 
@@ -52,7 +41,7 @@ class PostControllerTest {
                 .build());
 
         // when
-        ResponseEntity<PostResponse> result = testContainer.postController.getPostById(1);
+        ResponseEntity<PostResponse> result = testContainer.postController.getById(1);
 
         // then
         assertThat(result.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
@@ -89,7 +78,7 @@ class PostControllerTest {
                 .build());
 
         // when
-        ResponseEntity<PostResponse> result = testContainer.postController.updatePost(1, PostUpdate.builder()
+        ResponseEntity<PostResponse> result = testContainer.postController.update(1, PostUpdate.builder()
                 .content("helloworld :)")
                 .build());
 
@@ -114,7 +103,7 @@ class PostControllerTest {
         // when
         // then
         assertThatThrownBy(() -> {
-            testContainer.postController.getPostById(1);
+            testContainer.postController.getById(1);
         }).isInstanceOf(ResourceNotFoundException.class);
 
     }
