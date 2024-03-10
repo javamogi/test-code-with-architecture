f403f90a0becccb8807073e3c3ca4a239a79812a
javamogi <javamogi@gmail.com>
2024-03-10 10:22:03 +0900

    CertificationService 테스트 추가

---
+++ src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.user.service;
+
+import com.example.demo.mock.FakeMailSender;
+import com.example.demo.user.service.port.MailSender;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CertificationServiceTest {
+
+    @Test
+    void 이메일과_컨텐츠가_제데로_만들어져서_보내지는지_테스트한다() {
+        // given
+        FakeMailSender fakeMailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(fakeMailSender);
+
+        // when
+        certificationService.send("pakoh200@naver.com", 1, "aaaaaa-aaaa-aaaa-aaaa-aaaaaaaa");
+
+        //then
+        assertThat(fakeMailSender.email).isEqualTo("pakoh200@naver.com");
+        assertThat(fakeMailSender.title).isEqualTo("Please certify your email address");
+        assertThat(fakeMailSender.content).isEqualTo(
+                        "Please click the following link to certify your email address: http://localhost:8080/api/users/1/verify?certificationCode=aaaaaa-aaaa-aaaa-aaaa-aaaaaaaa");
+    }
+}
